30d7c5877c5740ac33195d5212ae4d5fb8aa8ec3
SUZJ <43955116+HZSUZJ@users.noreply.github.com>
2020-04-06 17:06:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cid=$(date "+%Y-%m-%d")
+sed -i "s/19.07-SNAPSHOT/19.07-SNAPSHOT-[${cid}]/g" package/default-settings/files/zzz-default-settings
 
 sed -i "22i\uci set network.lan.proto='dhcp'"  package/default-settings/files/zzz-default-settings
 sed -i '24i\uci commit network'  package/default-settings/files/zzz-default-settings
